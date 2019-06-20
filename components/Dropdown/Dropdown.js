bdce5778356f74ac814ab027d6799a174bcfcbec
Neha Patel <nehatr0n@Nehas-MacBook-Pro.local>
2019-06-20 14:40:05 -0700

    got dropdown to work, but did not use arrow function

--- components/Dropdown/Dropdown.js
+++ components/Dropdown/Dropdown.js
@@ -6,21 +6,18 @@ class Dropdown {
     // console.log(this.element);
     // Get the element with the ".dropdown-button" class found in the dropdown element (look at the HTML for context)
     this.button = this.element.querySelector('.dropdown-button');
-    console.log(this.button);
 
     // assign the reference to the ".dropdown-content" class found in the dropdown element
-    this.content;
+    this.content = this.element.querySelector('.dropdown-content');
     
     // Add a click handler to the button reference and call the toggleContent method.
-    this.button.addEventListener('click', () => {
-
-    })
+    this.button.addEventListener('click', this.toggleContent.bind(this));
   }
 
   toggleContent() {
     
     // Toggle the ".dropdown-hidden" class off and on
-    this.content;
+    this.content.classList.toggle('dropdown-hidden');
   }
 }
 
