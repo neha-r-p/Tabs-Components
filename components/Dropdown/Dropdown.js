3cd31d25460d0486070448ec8336de3e0a687e2d
Neha Patel <nehatr0n@Nehas-MacBook-Pro.local>
2019-06-20 14:22:22 -0700

    assigned this.button

--- components/Dropdown/Dropdown.js
+++ components/Dropdown/Dropdown.js
@@ -3,10 +3,11 @@ class Dropdown {
     
     // Assign this.element to the dropdown element
     this.element = element;
-    console.log(this.element);
+    // console.log(this.element);
     // Get the element with the ".dropdown-button" class found in the dropdown element (look at the HTML for context)
-    this.button = this.element.querySelector();
-    
+    this.button = this.element.querySelector('.dropdown-button');
+    console.log(this.button);
+
     // assign the reference to the ".dropdown-content" class found in the dropdown element
     this.content;
     
